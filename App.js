70dfb5a63efe80cc0ccf3d16caf5ccb62a360e6b
Gelya <gelya.ervine@student.umn.ac.id>
2025-04-30 15:07:02 +0700

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  PermissionsAndroid,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
 
 export default function App() {
+  const [uri, setUri] = useState("");
+  const openImagePicker = () => {
+    launchImageLibrary(
+      {
+        mediaType: "photo",
+        includeBase64: false,
+        maxHeight: 2000,
+        maxWidth: 2000,
+      },
+      handleResponse,
+    );
+  };
+
+  const handleCameraLaunch = () => {
+    launchCamera(
+      {
+        mediaType: "photo",
+        includeBase64: false,
+        maxHeight: 2000,
+        maxWidth: 2000,
+      },
+      handleResponse,
+    );
+  };
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: "Camera Permission",
+          message: "This app needs access to your camera to take photos.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK",
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Camera permission granted");
+        handleCameraLaunch();
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const handleResponse = (response) => {
+    if (response.didCancel) {
+      console.log("User cancelled image picker");
+    } else if (response.error) {
+      console.log("Image picker error: ", response.error);
+    } else if (response.assets && response.assets.length > 0) {
+      const imageUri = response.assets[0].uri;
+      setUri(imageUri);
+    } else {
+      console.log("No assets found in the response");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={requestCameraPermission} title="OPEN CAMERA" />
     </View>
   );
 }
@@ -13,8 +82,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
